3716b9ef0ed7d14c07f977283a5c6c8c4248f968
Vladimir Sumarov <summeroff@gmail.com>
2019-03-01 23:20:34 +0300

    switch to vs compiler from ninja-clang

--- ci/appveyor/sign_binaries.ps1
+++ ci/appveyor/sign_binaries.ps1
@@ -1,6 +1,6 @@
 Copy-Item `
-        -Path "build\slobs-updater.exe" `
-        -Destination "build\slobs-updater-signed-$env:BUILD_TYPE.exe"
+        -Path "$env:BUILD_DIR\$env:BUILD_TYPE\slobs-updater.exe" `
+        -Destination "$env:BUILD_DIR\$env:BUILD_TYPE\slobs-updater-signed-$env:BUILD_TYPE.exe"
 
 & secure-file\tools\secure-file `
         -decrypt ci\streamlabsp12.pfx.enc `
@@ -15,10 +15,10 @@ if ($LASTEXITCODE -ne 0) {
         sign /as /p "$env:STREAMLABS_SECRET" `
         /f ci\streamlabsp12.pfx `
         /d "Streamlabs OBS Updater" `
-        "build\slobs-updater-signed-$env:BUILD_TYPE.exe"
+        "$env:BUILD_DIR\$env:BUILD_TYPE\slobs-updater-signed-$env:BUILD_TYPE.exe"
 
 if ($LASTEXITCODE -ne 0) {
     exit 1
 }
 
-Push-AppveyorArtifact "build\slobs-updater-signed-$env:BUILD_TYPE.exe"
+Push-AppveyorArtifact "$env:BUILD_DIR\$env:BUILD_TYPE\slobs-updater-signed-$env:BUILD_TYPE.exe"
