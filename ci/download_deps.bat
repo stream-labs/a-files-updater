838db98d9aa036755a2fde6c63129b26f324401d
Vladimir Sumarov <summeroff@gmail.com>
2022-08-19 13:09:17 -0700

    update build script

--- ci/download_deps.bat
+++ ci/download_deps.bat
@@ -1,7 +1,12 @@
 mkdir "%DEPS_LOCAL_PATH%"
+set ZLIB_DIST_NAME=zlib-1.2.11-static-mt
+set OPENSSL_DIST_NAME=openssl-1.1.1c-x64
+set BOOST_DIST_NAME=boost-vc143-1_79_0-bin
+set DEPS_DIST_URI=https://s3-us-west-2.amazonaws.com/streamlabs-obs-updater-deps
+
 curl -kLO "%DEPS_DIST_URI%/%ZLIB_DIST_NAME%.7z" -f --retry 5
 curl -kLO "%DEPS_DIST_URI%/%OPENSSL_DIST_NAME%.7z" -f --retry 5
 curl -kLO "%DEPS_DIST_URI%/%BOOST_DIST_NAME%.7z" -f --retry 5
-7z x "%ZLIB_DIST_NAME%.7z" -o"%DEPS_LOCAL_PATH%\%ZLIB_DIST_NAME%" -y
-7z x "%OPENSSL_DIST_NAME%.7z" -o"%DEPS_LOCAL_PATH%\%OPENSSL_DIST_NAME%" -y
-7z x "%BOOST_DIST_NAME%.7z" -o"%DEPS_LOCAL_PATH%\%BOOST_DIST_NAME%" -y
+7z x "%ZLIB_DIST_NAME%.7z" -o"%DEPS_LOCAL_PATH%\zlib" -y 
+7z x "%OPENSSL_DIST_NAME%.7z" -o"%DEPS_LOCAL_PATH%\openssl" -y
+7z x "%BOOST_DIST_NAME%.7z" -o"%DEPS_LOCAL_PATH%\boost" -y
