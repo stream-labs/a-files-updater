3f0e43e623a80f93e2811006b1f539615af404fa
Vladimir Sumarov <summeroff@gmail.com>
2022-09-01 15:29:08 -0700

    clean up code

--- ci/localization_set_translations.cmd
+++ ci/localization_set_translations.cmd
@@ -1,14 +1,10 @@
 REM get tools 
 call ci\localization_get_tools.cmd
 
-set locales_list="ar_SA","cs_CZ","da_DK","de_DE","en_US","es_ES","fr_FR","hu_HU","id_ID","it_IT","ja_JP","ko_KR","mk_MK","nl_NL","pl_PL","pt_BR","pt_PT","ru_RU","sk_SK","sl_SI","sv_SE","th_TH","tr_TR","vi_VN","zh_CN","zh_TW"
-set GETTEXT_BIN=build\gettext_dist\bin\
-
 REM dump strings need to be translated from source code 
 %GETTEXT_BIN%xgettext.exe --keyword=translate:1,1t src\main.cc src\update-client.cc -d messages -p locale
 del locale\\messages.pot
 rename locale\\messages.po messages.pot
-
 
 REM make sure that the locale directory exists for each locale to be able to save files to it
 for %%F in (%locales_list%) do if not exist "locale\%%~F\" mkdir "locale\%%~F\\LC_MESSAGES"
